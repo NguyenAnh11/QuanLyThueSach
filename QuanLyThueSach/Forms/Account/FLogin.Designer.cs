55485abba5c2b4a94e3a96556358f6af459ac62f
Anh <anhnguyenviet11299@gmail.com>
2021-10-17 14:42:35 +0700

    remove signup button and method use when signup raise event click

--- QuanLyThueSach/Forms/Account/FLogin.Designer.cs
+++ QuanLyThueSach/Forms/Account/FLogin.Designer.cs
@@ -30,24 +30,23 @@ namespace QuanLyThueSach.Forms.Account
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.panel2 = new System.Windows.Forms.Panel();
-            this.panel3 = new System.Windows.Forms.Panel();
-            this.labelUsername = new System.Windows.Forms.Label();
-            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.btnSignIn = new System.Windows.Forms.Button();
             this.panel4 = new System.Windows.Forms.Panel();
             this.txtPassword = new System.Windows.Forms.TextBox();
             this.labelPassword = new System.Windows.Forms.Label();
-            this.panel5 = new System.Windows.Forms.Panel();
-            this.btnSignUp = new System.Windows.Forms.Button();
-            this.btnSignIn = new System.Windows.Forms.Button();
-            this.btnExit = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.labelUsername = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.panel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.panel2.SuspendLayout();
-            this.panel3.SuspendLayout();
-            this.panel4.SuspendLayout();
             this.panel5.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
@@ -58,16 +57,6 @@ namespace QuanLyThueSach.Forms.Account
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(484, 292);
             this.panel1.TabIndex = 0;
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Image = global::QuanLyThueSach.Properties.Resources.default_avatar;
-            this.pictureBox1.Location = new System.Drawing.Point(14, 30);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(168, 226);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
             // 
             // panel2
             // 
@@ -80,30 +69,33 @@ namespace QuanLyThueSach.Forms.Account
             this.panel2.Size = new System.Drawing.Size(293, 226);
             this.panel2.TabIndex = 1;
             // 
-            // panel3
+            // btnExit
             // 
-            this.panel3.Controls.Add(this.txtUsername);
-            this.panel3.Controls.Add(this.labelUsername);
-            this.panel3.Location = new System.Drawing.Point(0, 0);
-            this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(293, 52);
-            this.panel3.TabIndex = 0;
+            this.btnExit.Location = new System.Drawing.Point(208, 188);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 3;
+            this.btnExit.Text = "Thoát";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
             // 
-            // labelUsername
+            // panel5
             // 
-            this.labelUsername.AutoSize = true;
-            this.labelUsername.Location = new System.Drawing.Point(14, 22);
-            this.labelUsername.Name = "labelUsername";
-            this.labelUsername.Size = new System.Drawing.Size(81, 13);
-            this.labelUsername.TabIndex = 0;
-            this.labelUsername.Text = "Tên đăng nhập";
+            this.panel5.Controls.Add(this.btnSignIn);
+            this.panel5.Location = new System.Drawing.Point(0, 116);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(293, 49);
+            this.panel5.TabIndex = 2;
             // 
-            // txtUsername
+            // btnSignIn
             // 
-            this.txtUsername.Location = new System.Drawing.Point(115, 15);
-            this.txtUsername.Name = "txtUsername";
-            this.txtUsername.Size = new System.Drawing.Size(169, 20);
-            this.txtUsername.TabIndex = 1;
+            this.btnSignIn.Location = new System.Drawing.Point(17, 14);
+            this.btnSignIn.Name = "btnSignIn";
+            this.btnSignIn.Size = new System.Drawing.Size(75, 23);
+            this.btnSignIn.TabIndex = 1;
+            this.btnSignIn.Text = "Đăng nhập";
+            this.btnSignIn.UseVisualStyleBackColor = true;
+            this.btnSignIn.Click += new System.EventHandler(this.btnSignIn_Click);
             // 
             // panel4
             // 
@@ -130,44 +122,40 @@ namespace QuanLyThueSach.Forms.Account
             this.labelPassword.TabIndex = 0;
             this.labelPassword.Text = "Mật khẩu";
             // 
-            // panel5
+            // panel3
             // 
-            this.panel5.Controls.Add(this.btnSignIn);
-            this.panel5.Controls.Add(this.btnSignUp);
-            this.panel5.Location = new System.Drawing.Point(0, 116);
-            this.panel5.Name = "panel5";
-            this.panel5.Size = new System.Drawing.Size(293, 49);
-            this.panel5.TabIndex = 2;
+            this.panel3.Controls.Add(this.txtUsername);
+            this.panel3.Controls.Add(this.labelUsername);
+            this.panel3.Location = new System.Drawing.Point(0, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(293, 52);
+            this.panel3.TabIndex = 0;
             // 
-            // btnSignUp
+            // txtUsername
             // 
-            this.btnSignUp.Location = new System.Drawing.Point(17, 16);
-            this.btnSignUp.Name = "btnSignUp";
-            this.btnSignUp.Size = new System.Drawing.Size(75, 23);
-            this.btnSignUp.TabIndex = 0;
-            this.btnSignUp.Text = "Đăng ký";
-            this.btnSignUp.UseVisualStyleBackColor = true;
-            this.btnSignUp.Click += new System.EventHandler(this.btnSignUp_Click);
+            this.txtUsername.Location = new System.Drawing.Point(115, 15);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(169, 20);
+            this.txtUsername.TabIndex = 1;
             // 
-            // btnSignIn
+            // labelUsername
             // 
-            this.btnSignIn.Location = new System.Drawing.Point(208, 16);
-            this.btnSignIn.Name = "btnSignIn";
-            this.btnSignIn.Size = new System.Drawing.Size(75, 23);
-            this.btnSignIn.TabIndex = 1;
-            this.btnSignIn.Text = "Đăng nhập";
-            this.btnSignIn.UseVisualStyleBackColor = true;
-            this.btnSignIn.Click += new System.EventHandler(this.btnSignIn_Click);
+            this.labelUsername.AutoSize = true;
+            this.labelUsername.Location = new System.Drawing.Point(14, 22);
+            this.labelUsername.Name = "labelUsername";
+            this.labelUsername.Size = new System.Drawing.Size(81, 13);
+            this.labelUsername.TabIndex = 0;
+            this.labelUsername.Text = "Tên đăng nhập";
             // 
-            // btnExit
+            // pictureBox1
             // 
-            this.btnExit.Location = new System.Drawing.Point(208, 188);
-            this.btnExit.Name = "btnExit";
-            this.btnExit.Size = new System.Drawing.Size(75, 23);
-            this.btnExit.TabIndex = 3;
-            this.btnExit.Text = "Thoát";
-            this.btnExit.UseVisualStyleBackColor = true;
-            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            this.pictureBox1.Image = global::QuanLyThueSach.Properties.Resources.default_avatar;
+            this.pictureBox1.Location = new System.Drawing.Point(14, 30);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(168, 226);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
             // 
             // FLogin
             // 
@@ -179,13 +167,13 @@ namespace QuanLyThueSach.Forms.Account
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Đăng nhập";
             this.panel1.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.panel2.ResumeLayout(false);
-            this.panel3.ResumeLayout(false);
-            this.panel3.PerformLayout();
+            this.panel5.ResumeLayout(false);
             this.panel4.ResumeLayout(false);
             this.panel4.PerformLayout();
-            this.panel5.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -197,7 +185,6 @@ namespace QuanLyThueSach.Forms.Account
         private System.Windows.Forms.Button btnExit;
         private System.Windows.Forms.Panel panel5;
         private System.Windows.Forms.Button btnSignIn;
-        private System.Windows.Forms.Button btnSignUp;
         private System.Windows.Forms.Panel panel4;
         private System.Windows.Forms.TextBox txtPassword;
         private System.Windows.Forms.Label labelPassword;
